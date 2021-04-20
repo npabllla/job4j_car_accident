644d4d703ec55687e3dd9dc3513c4675e19b1ccf
Kirill <npabllla@gmail.com>
2021-04-20 15:00:56 +0300

    1. Загрузка данных в вид [#492616]

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -4,10 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
+        List<String> strings = List.of("Один", "Два", "Три");
+        model.addAttribute("numbers", strings);
         return "index";
     }
 }
