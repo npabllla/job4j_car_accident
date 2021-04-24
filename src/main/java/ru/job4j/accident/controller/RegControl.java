9c98dc9fcb156222dadc91b79712a912ac2bbe12
Kirill <npabllla@gmail.com>
2021-04-24 12:55:11 +0300

    2. Регистрация пользователя [#492630]

--- src/main/java/ru/job4j/accident/controller/RegControl.java
+++ src/main/java/ru/job4j/accident/controller/RegControl.java
@@ -12,9 +12,6 @@ import ru.job4j.accident.model.User;
 import ru.job4j.accident.repository.AuthorityRepository;
 import ru.job4j.accident.repository.UserRepository;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class RegControl {
 
@@ -30,17 +27,14 @@ public class RegControl {
 
     @PostMapping("/reg")
     public String save(@ModelAttribute User user, Model model) {
-        List<User> users = new ArrayList<>();
-        this.users.findAll().forEach(users::add);
-        if (users.stream().anyMatch(e -> e.getUsername().equals(user.getUsername()))) {
+        if (users.findByUsername(user.getUsername()).isPresent()) {
             model.addAttribute("error", "Данный логин уже используется");
             return "reg";
-        } else {
-            user.setEnabled(true);
-            user.setPassword(encoder.encode(user.getPassword()));
-            user.setAuthority(authorities.findByAuthority("ROLE_USER"));
-            this.users.save(user);
         }
+        user.setEnabled(true);
+        user.setPassword(encoder.encode(user.getPassword()));
+        user.setAuthority(authorities.findByAuthority("ROLE_USER"));
+        this.users.save(user);
         return "redirect:/login";
     }
 
--- src/main/java/ru/job4j/accident/repository/UserRepository.java
+++ src/main/java/ru/job4j/accident/repository/UserRepository.java
@@ -3,5 +3,8 @@ package ru.job4j.accident.repository;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.accident.model.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
 }
